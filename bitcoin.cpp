9bee6b4ed3dff23e8957c6993edc6a27c2074b67
Unknown <theholyroger@theholyroger.com>
2018-06-17 03:46:42 +0100

    rogerd

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -296,7 +296,7 @@ bool TestNode(const CService &cip, int &ban, int &clientV, std::string &clientSV
   }
 }
 
-/
+
 int main(void) {
   CService ip("explorer.theholyroger.com", 9663, true);
   vector<CAddress> vAddr;
