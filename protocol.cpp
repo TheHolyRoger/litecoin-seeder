ed5ebb816c4e77d9f1222ac3fdb5914d417c2e3a
Unknown <theholyroger@theholyroger.com>
2018-06-17 01:24:37 +0100

    Rogerd

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xfb, 0xc0, 0xb6, 0xdb };
+unsigned char pchMessageStart[4] = { 0xfc, 0xc2, 0xc4, 0xeb };
 
 CMessageHeader::CMessageHeader()
 {
